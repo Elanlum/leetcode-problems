bd47e3086413b6aeb8d8cb3ba48dddcd87a8b268
Горбатенко Роман Анатольевич <ragorbatenko@gid.ru>
2024-01-21 12:29:49 +0300

    Problem 100 (Easy)

---
+++ src/main/problem_100/SameTree.java
@@ -0,0 +1,31 @@
+package main.problem_100;
+
+
+public class SameTree {
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode leftp = new TreeNode(2);
+        TreeNode rightp = new TreeNode(3);
+        TreeNode p = new TreeNode(1, leftp, rightp);
+
+        TreeNode leftq = new TreeNode(2);
+        TreeNode rightq = new TreeNode(3);
+        TreeNode q = new TreeNode(1, leftq, rightq);
+        System.out.println(isSameTree(p, q));
+    }
+}
