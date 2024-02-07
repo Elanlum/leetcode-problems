51991103ccf8aa211517c26694ffb2d753a86930
Elanlum <piratone94@mail.ru>
2024-02-07 23:58:51 +0300

    Problem 653 (Easy)

--- src/problem_653/RecursiveSolution.java
+++ src/problem_653/RecursiveSolution.java
@@ -2,10 +2,9 @@ package problem_653;
 
 public class RecursiveSolution {
 
-    private static TreeNode globalRoot = null;
+    private TreeNode globalRoot = null;
 
-    public static boolean findTarget(TreeNode root, int k) {
-        // корень пустой
+    public boolean findTarget(TreeNode root, int k) {
         if (root == null) {
             return false;
         }
@@ -14,78 +13,28 @@ public class RecursiveSolution {
             globalRoot = root;
         }
 
-        if (binarySearch(globalRoot, k - root.val, root.val)) {
+        TreeNode node = binarySearch(globalRoot, k - root.val);
+
+        if (node != null && node != root) {
             return true;
         }
 
-        if (findTarget(root.right, k)) {
-            return true;
-        }
-
-        return findTarget(root.left, k);
+        return findTarget(root.left, k) || findTarget(root.right, k);
     }
 
-    private static boolean binarySearch(TreeNode node, int find, int nonFind) {
+    private static TreeNode binarySearch(TreeNode node, int find) {
         if (node == null) {
-            return false;
+            return null;
         }
 
-        if (find == nonFind) {
-            return false;
+        if (find < node.val) {
+            return binarySearch(node.left, find);
         }
 
-        if (node.val == find) {
-            return true;
+        if (find > node.val) {
+            return binarySearch(node.right, find);
         }
 
-        // оба листа есть
-        if (node.left != null && node.right != null) {
-            return binarySearch(node.left, find, nonFind) || binarySearch(node.right, find, nonFind);
-        }
-
-        // только левый
-        if (node.left != null) {
-            return binarySearch(node.left, find, nonFind);
-        }
-
-        // только правый
-        if (node.right != null) {
-            return binarySearch(node.right, find, nonFind);
-        }
-
-        return false;
+        return node;
     }
-
-//    public static void main(String[] args) {
-//        TreeNode one = new TreeNode(1, null, null);
-//        TreeNode minusFour = new TreeNode(-4, null, null);
-////        TreeNode seven = new TreeNode(7, null, null);
-//        TreeNode zero = new TreeNode(0, minusFour, one);
-//        TreeNode three = new TreeNode(3, null, null);
-//
-//        TreeNode root = new TreeNode(2, zero, three);
-//
-//        System.out.println(Solution.findTarget(root, -1));
-//    }
-
-//    public static void main(String[] args) {
-//        TreeNode three = new TreeNode(3, null, null);
-//        TreeNode root = new TreeNode(2, null, three);
-//
-//        System.out.println(Solution.findTarget(root, 6));
-//    }
-
-//    public static void main(String[] args) {
-//        TreeNode two = new TreeNode(2, null, null);
-//        TreeNode root = new TreeNode(1, null, two);
-//
-//        System.out.println(Solution.findTarget(root, 2));
-//    }
-
-//    public static void main(String[] args) {
-//        TreeNode two = new TreeNode(2, null, null);
-//        TreeNode root = new TreeNode(1, null, two);
-//
-//        System.out.println(Solution.findTarget(root, 2));
-//    }
 }
